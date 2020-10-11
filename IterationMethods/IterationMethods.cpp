c708aa29e6928a6cdf608ec1ec098e3b2bf153ab
kovila77 <354981@mail.ru>
2020-10-11 10:28:42 +0500

    add comments in main

--- IterationMethods/IterationMethods.cpp
+++ IterationMethods/IterationMethods.cpp
@@ -4,17 +4,19 @@
 #include "Matrix.h"
 using namespace std;
 
-const bool PRIN_TO_FILE = false;
+const bool PRIN_TO_FILE = true;
 
 int main() {
 	string variant = "input1";
-	ifstream fin("../" + variant + ".txt");
 
 	int n;
+
+	// Input n, A, b
+	ifstream fin("../" + variant + ".txt");
 	fin >> n;
 	Matrix matr(n);
-	matr.input(fin);
 	Vector b(n);
+	matr.input(fin);
 	b.input(fin);
 	fin.close();
 
@@ -26,22 +28,26 @@ int main() {
 	b.print(out);
 
 	out << "A" << endl;
-	matr.print(cout);
+	matr.print(out);
 
-	cout << "Third norm of a matrix " << matr.get_third_norm() << std::endl << std::endl;
+	out << "Third norm of a matrix " << matr.get_third_norm() << std::endl << std::endl;
 
 	Vector result(n);
+
 	result = matr.simple_iteration_method(b, out);
-	result.print(std::cout);
+	result.print(out);
 	out << endl;
+
 	result = matr.gradient_steepest_descent_method(b, out);
-	result.print(std::cout);
+	result.print(out);
 	out << endl;
+
 	result = matr.sor_method(b, out);
-	result.print(std::cout);
+	result.print(out);
 	out << endl;
+
 	result = matr.conjugate_gradient_method(b, out);
-	result.print(std::cout);
+	result.print(out);
 	out << endl;
 
 	fout.close();
